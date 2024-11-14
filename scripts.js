c9694b9a341db23c248aefb93f02bfa053979860
Vihaa-T <160472877+Vihaa-T@users.noreply.github.com>
2024-11-14 22:11:04 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+console.log("JavaScript file loaded");
+
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    alert('Thank you for reaching out! We will contact you shortly.');
+}
