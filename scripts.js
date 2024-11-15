1a4e132352fa8de30ae62a3b5523b0f117b49403
Vihaa-T <160472877+Vihaa-T@users.noreply.github.com>
2024-11-15 11:55:53 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,7 @@ function handleSubmit(event) {
     event.preventDefault(); // Prevent the form from submitting
     alert('Thank you for reaching out! We will contact you shortly.');
 }
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+}
